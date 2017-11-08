546af5a8966903c18c350c7668f35d1fc942733a
Andrew Hearin <andrew.hearin@yale.edu>
2017-11-08 09:39:32 -0600

    Completed notebook with formal statement of challenge problem #1

--- challenge_problem1.py
+++ challenge_problem1.py
@@ -8,17 +8,30 @@ from astropy.utils.misc import NumpyRNGContext
 from halotools.empirical_models import Cacciato09Cens
 
 
-rband_dict1 = dict(gamma_1=3.273, log_L_0=9.935, log_M_1=11.07, gamma_2=0.255)
-gband_dict1 = dict(gamma_1=2.5, log_L_0=9., log_M_1=10.7, gamma_2=0.55)
+rband_dict_source = dict(gamma_1=3.273, log_L_0=9.935, log_M_1=11.07, gamma_2=0.255)
+rband_dict_target = dict(gamma_1=3.073, log_L_0=9.635, log_M_1=11.27, gamma_2=0.285)
+gband_dict_source = dict(gamma_1=2.85, log_L_0=9.4, log_M_1=10.7, gamma_2=0.55)
+gband_dict_target = dict(gamma_1=3.00, log_L_0=9.5, log_M_1=10.5, gamma_2=0.45)
+
 default_sqrt_cov = np.array(((0.05, 0.05), (0.05, 0.15)))
 
 
-__all__ = ('generate_mock', )
+__all__ = ('generate_mock', 'generate_source_galaxies', 'generate_target_galaxies')
 
 
-def generate_mock(num_halos=int(1e6), seed=None,
-            rband_dict=rband_dict1, gband_dict=gband_dict1,
-            sqrt_cov=default_sqrt_cov):
+def generate_source_galaxies():
+    """
+    """
+    return generate_mock(rband_dict_source, gband_dict_source, default_sqrt_cov)
+
+
+def generate_target_galaxies():
+    """
+    """
+    return generate_mock(rband_dict_target, gband_dict_target, default_sqrt_cov)
+
+
+def generate_mock(rband_dict, gband_dict, sqrt_cov, num_halos=int(1e6), seed=43):
     """
     """
     #  Initialize the table storing the galaxies
@@ -42,8 +55,10 @@ def generate_mock(num_halos=int(1e6), seed=None,
 
     #  Define a transformation matrix U so that U*U^T = cov
     U = np.dot(evecs, np.diag(np.sqrt(evals)))
-    a = norm.rvs(size=num_halos)
-    b = norm.rvs(size=num_halos)
+    with NumpyRNGContext(seed):
+        a = norm.rvs(size=num_halos)
+    with NumpyRNGContext(seed+1):
+        b = norm.rvs(size=num_halos)
     X = np.array((a, b)).reshape((2, num_halos))
 
     #  Use the transformation matrix U to calculate correlated variables (c, d)
